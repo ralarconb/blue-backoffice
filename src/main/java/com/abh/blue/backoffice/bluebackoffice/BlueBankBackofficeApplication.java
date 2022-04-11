1087f3552f64ccb7a43b84e874e17522b078516f
ralarconb <ralarconbazan@gmail.com>
2022-04-11 04:26:23 -0500

    .

---
+++ src/main/java/com/abh/blue/backoffice/bluebackoffice/BlueBankBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.abh.blue.backoffice.bluebackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBankBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBankBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abh/blue/backoffice/bluebackoffice/BlueBankBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abh.blue.backoffice.bluebackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBankBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
